314b84521db3df752d0c1b821d3178bb5808920d
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-07-30 11:25:13 -0400

    Add variables

---
+++ section3-fundamentals/variables.php
@@ -0,0 +1,30 @@
+<?php 
+  // STRINGS VARIABLES
+  $name = 'Amanda Lai';
+  $number = 2;
+
+  $name = 'Brian';
+
+  $NAME = "Carl";
+  $Name = 'Dan';
+
+  // Snake Case
+  $full_name = "Erin";
+  // Camel Case
+  $fullName = 'George';
+  // PascalCase
+  $FullName = 'Hannah';
+
+  var_dump($name);
+
+  // INTEGER VARIABLES
+  $number = 2;
+  $float = 2.5;
+
+  var_dump($number);
+  var_dump($float);
+
+
+  $value1 = $value2 = $value3= 'same';
+  var_dump($value1);
+?>
