59d6272ab62639b1b0d0c1641d8164750e18d0ea
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-08-01 15:26:27 -0400

    Create if_statements.php

---
+++ section4-Ifs/if_statements.php
@@ -0,0 +1,16 @@
+<?php 
+
+  $num1 = 10;
+  $num2 = 20;
+
+  if ($num1 < $num2) {
+    echo "IT IS";
+  } elseif ($num1 > $num2) {
+    echo "NOPE";
+  }
+  else { 
+    echo "IT IS NOT";
+  }
+  echo "<br>";
+  echo "NEXT LINE";
+?>
