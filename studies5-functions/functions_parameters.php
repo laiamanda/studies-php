052032a4fd3815f26d31962707c1739adfdc0e4d
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-08-10 11:44:32 -0400

    Add functions_parameters.php

---
+++ studies5-functions/functions_parameters.php
@@ -0,0 +1,14 @@
+<?php 
+    function calculateRectangleArea($length, $width) {
+        return $length * $width;
+    }
+
+    echo calculateRectangleArea(10,5);
+
+    function calculateSum($a, $b) {
+        return $a + $b;
+    }
+
+    $sum = calculateSum(5, 10);
+    echo $sum;
+?>
