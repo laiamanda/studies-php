92f36834af690613f259d8740d4bd01ee83d2ac4
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-08-13 10:47:14 -0400

    Create arrays.php

---
+++ section7-arrays/arrays.php
@@ -0,0 +1,12 @@
+<?php 
+    // Index Array
+    $fruits = ["Apple", "Banana", "Orange"];
+    // Access elements in array
+    echo $fruits[0];
+
+    // Add new element to the array
+    $fruits[] = "Mango";
+
+    echo "<br>";
+    var_dump($fruits);
+?>
