e6d42985f1aa691398aaa3ceb3a136034a21322c
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-08-20 10:20:59 -0400

    Create index.php

---
+++ section9-forms/forms/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <title>Simple Form</title>
+    </head>
+    <body>
+        <h2> Basic Form</h2>
+        <form action="process.php" method="POST">
+            <input name="username" type="text" placeholder="Enter Username">
+            <input name="password" type="text" placeholder="Enter Password">
+            <input type="submit">
+        </form>
+    </body>
+</html>
