8e4462fbe557ced27cfdffbdcab79488337e0560
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-08-22 10:39:51 -0400

    Create index.php

---
+++ section9-forms/search/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <title>Search</title>
+    </head>
+    <body>
+        <h2> Search Form </h2>
+        <form action="search.php" method="get">
+            Search: <input name="query" type="text">
+            <input type="submit" value="Search">
+        </form>
+    </body>
+</html>
