a688f3fdff2af12b19cb6f52fb3fc992b50cfb9a
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-08-24 12:03:06 -0400

    Create index.php

---
+++ section9-forms/validation/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Validation</title>
+</head>
+<body>
+
+<form action="process.php" method="post">
+    
+<input type="text" name="username" placeholder="Enter Username">
+<input type="text"  name="email" placeholder="Enter Email"><br>
+<input type="submit" name="submit">
+    
+    
+</form>
+
+
+
+</body>
+</html>
