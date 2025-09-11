9944e8ef2b0e3014dfe009bc302ef009ec3b85f3
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-09-11 10:15:42 -0400

    Create index.php

---
+++ section13-loginApp/session/index.php
@@ -0,0 +1,12 @@
+<?php 
+    session_start(); // Session ID
+
+    $_SESSION['username']='Amanda';
+
+    // echo $_SESSION['username'];
+    
+?>
+
+<a href="about.php">About</a>
+<br>
+<a href="content.php">Content</a>
